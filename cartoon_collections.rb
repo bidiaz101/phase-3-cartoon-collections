1d7fb8227c98e383613be45cae25557a5f64c0f9
Brian Diaz <bidiaz101@gmail.com>
2021-10-16 15:15:35 -0500

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_arr)
+  dwarf_arr.each.with_index(1) {|dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call_arr)
+  call_arr.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_arr)
+  call_arr.filter {|call| call.length > 4}.length > 0
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_arr.find do |food|
+    cheese_types.find {|cheese| cheese == food}
+  end
 end
